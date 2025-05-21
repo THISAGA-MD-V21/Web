5e07951fc09c86b46c59b4a6e26b6ef034dbf9af
THISAGA-MD-V21 <thisagapanel@gmail.com>
2025-05-21 18:25:52 +0530

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("contact-form").addEventListener("submit", function (e) {
-  e.preventDefault();
-  document.getElementById("form-response").innerText = "Message sent! (Simulation)";
-});
